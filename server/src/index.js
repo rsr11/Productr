7f5b3e3fdce071c2f8463a19906ca2737ef69e32
Rajeshwar <rsr45411@gmail.com>
2026-01-26 13:54:19 +0530

    changes in server file

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+app.use(cors({origin:"https://productrsr.netlify.app", credentials:true}));
 
 initCloudinary();
 
